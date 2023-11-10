e12ebfd0e03b43d3db1587115480ac8d91bca2a7
艾梦 <HighCWu@163.com>
2023-11-10 14:12:24 +0000

    update

--- extract_docker_image.sh
+++ extract_docker_image.sh
@@ -1,8 +1,7 @@
-mkdir -p /root/tmp
-cd /root/tmp
 git clone https://github.com/jjlin/docker-image-extract
 cd docker-image-extract
 ./docker-image-extract -o ../output imiell/bash2py
 cd ..
-cp -r output/root/bin/* /root/bin/
-cp -r output/opt/* /opt/
+mkdir -p /root/bin/ /opt/
+cp output/root/bin/bash2pyengine /root/bin/bash2pyengine
+cp -r output/opt/bash2py-3.5 /opt/
